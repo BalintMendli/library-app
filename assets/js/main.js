bb32e2b441e368e9d3ce3e377974a9b24fe9bcf5
zenott <mblevel@gmail.com>
2019-06-09 20:25:24 +0200

    Add addBook function

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,53 +14,76 @@ function libraryMain() {
   addBookToLibrary('The Catcher in the Rye', 'J.D. Salinger', true);
   addBookToLibrary('The Brothers Karamazov', 'Fyodor Dostoevsky');
   addBookToLibrary('Ulysses', 'James Joyce');
-  console.log(myLibrary);
+
+  const container = document.querySelector('#book-container');
+  [...container.childNodes].forEach(el => el.remove());
+
+  function addBookNode(book, i) {
+    const bookNode = document.createElement('div');
+    bookNode.classList.add('book');
+    bookNode.setAttribute('data-number', i);
+
+    const title = document.createElement('h2');
+    title.textContent = book.title;
+    title.classList.add('book-title');
+    bookNode.appendChild(title);
+
+    const author = document.createElement('p');
+    author.textContent = book.author;
+    author.classList.add('book-author');
+    bookNode.appendChild(author);
+
+    const read = document.createElement('p');
+    read.textContent = book.read ? 'read' : 'not read';
+    read.classList.add('book-read');
+    function updateRead() {
+      myLibrary[i].read = !myLibrary[i].read;
+      const readNode = document.querySelector(
+        `[data-number='${i}'] .book-read`
+      );
+      readNode.textContent = myLibrary[i].read ? 'read' : 'not read';
+    }
+    read.addEventListener('click', updateRead);
+    bookNode.appendChild(read);
+
+    const remove = document.createElement('p');
+    remove.textContent = 'Remove book';
+    remove.classList.add('book-remove');
+    function removeBook() {
+      myLibrary.splice(i, 1);
+      document.querySelector(`[data-number='${i}']`).remove();
+    }
+    remove.addEventListener('click', removeBook);
+    bookNode.appendChild(remove);
+
+    container.appendChild(bookNode);
+  }
 
   function render() {
-    const container = document.querySelector('#book-container');
-    [...container.childNodes].forEach(el => el.remove());
-
-    myLibrary.forEach((book, i) => {
-      const bookNode = document.createElement('div');
-      bookNode.classList.add('book');
-      bookNode.setAttribute('data-number', i);
-
-      const title = document.createElement('h2');
-      title.textContent = book.title;
-      title.classList.add('book-title');
-      bookNode.appendChild(title);
-
-      const author = document.createElement('p');
-      author.textContent = book.author;
-      author.classList.add('book-author');
-      bookNode.appendChild(author);
-
-      const read = document.createElement('p');
-      read.textContent = book.read ? 'read' : 'not read';
-      read.classList.add('book-read');
-      function updateRead() {
-        myLibrary[i].read = !myLibrary[i].read;
-        const readNode = document.querySelector(
-          `[data-number='${i}'] .book-read`
-        );
-        readNode.textContent = myLibrary[i].read ? 'read' : 'not read';
-      }
-      read.addEventListener('click', updateRead);
-      bookNode.appendChild(read);
-
-      const remove = document.createElement('p');
-      remove.textContent = 'Remove book';
-      remove.classList.add('book-remove');
-      function removeBook() {
-        myLibrary.splice(i, 1);
-        document.querySelector(`[data-number='${i}']`).remove();
-      }
-      remove.addEventListener('click', removeBook);
-      bookNode.appendChild(remove);
-
-      container.appendChild(bookNode);
-    });
+    myLibrary.forEach((book, i) => addBookNode(book, i));
   }
+
+  document.querySelector('#new-book').addEventListener('click', () => {
+    document.querySelector('.form').classList.toggle('hide');
+  });
+
+  function addBook() {
+    const title = document.querySelector('#title-input').value;
+    const author = document.querySelector('#author-input').value;
+    const read = document.querySelector('#read-input').checked;
+    if (title && author) {
+      const book = new Book(title, author, read);
+      myLibrary.push(book);
+      addBookNode(book, myLibrary.length);
+      document.querySelector('.form').classList.add('hide');
+      document.querySelector('#title-input').value = '';
+      document.querySelector('#author-input').value = '';
+      document.querySelector('#read-input').checked = false;
+    } else {
+      alert("Title and author can't be empty.");
+    }
+  }
+  document.querySelector('#add-book').addEventListener('click', addBook);
 
   render();
 }
