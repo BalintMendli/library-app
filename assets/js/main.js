fc7b5dcd28e1b5240092d304c7b3c8fbeccf9c96
zenott <mblevel@gmail.com>
2019-06-20 18:47:58 +0200

    Refactor code

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,59 +8,17 @@ function libraryMain() {
     this.read = read;
   }
 
-  Book.prototype.render = function(parentNode) {
-    const i = parentNode.childElementCount;
-    const bookNode = document.createElement('div');
-    bookNode.classList.add('book');
-    bookNode.setAttribute('data-number', i);
-
-    const titleNode = document.createElement('h2');
-    titleNode.textContent = this.title;
-    titleNode.classList.add('book-title');
-    bookNode.appendChild(titleNode);
-
-    const authorNode = document.createElement('p');
-    authorNode.textContent = 'by ' + this.author;
-    authorNode.classList.add('book-author');
-    bookNode.appendChild(authorNode);
-
-    const pagesNode = document.createElement('p');
-    pagesNode.textContent = this.pages + ' pages';
-    pagesNode.classList.add('book-pages');
-    bookNode.appendChild(pagesNode);
-
-    const readNode = document.createElement('p');
-    readNode.textContent = 'Read: ';
-    readNode.classList.add('book-read');
-    if (this.read) readNode.classList.add('checked');
-    function updateRead() {
-      myLibrary[i].read = !myLibrary[i].read;
-      storeLibrary();
-      const readNodeToChange = document.querySelector(
-        `[data-number='${i}'] .book-read`
-      );
-      if (myLibrary[i].read) {
-        readNodeToChange.classList.add('checked');
-      } else {
-        readNodeToChange.classList.remove('checked');
-      }
-    }
-    readNode.addEventListener('click', updateRead);
-    bookNode.appendChild(readNode);
-
-    const removeNode = document.createElement('p');
-    removeNode.textContent = 'Remove book';
-    removeNode.classList.add('book-remove', 'button');
-    function removeBook() {
-      myLibrary.splice(i, 1);
-      storeLibrary();
-      renderLibrary();
-    }
-    removeNode.addEventListener('click', removeBook);
-    bookNode.appendChild(removeNode);
-
-    container.appendChild(bookNode);
+  Book.prototype.toggleRead = function() {
+    this.read = !this.read;
   };
+
+  function addBookToLibrary(title, author, pages, read) {
+    myLibrary.push(new Book(title, author, pages, read));
+  }
+
+  function removeBook(i) {
+    myLibrary.splice(i, 1);
+  }
 
   const initialBooks = [
     ['The Catcher in the Rye', 'J.D. Salinger', 554, true],
@@ -83,31 +41,73 @@ function libraryMain() {
     addBookToLibrary(book.title, book.author, book.pages, book.read)
   );
 
-  function storeLibrary() {
-    localStorage.setItem('library', JSON.stringify(myLibrary));
-  }
+  function renderBook(book, parentNode) {
+    const i = parentNode.childElementCount;
+    const bookNode = document.createElement('div');
+    bookNode.classList.add('book');
+    bookNode.setAttribute('data-number', i);
 
-  function addBookToLibrary(title, author, pages, read) {
-    myLibrary.push(new Book(title, author, pages, read));
-    storeLibrary();
+    const titleNode = document.createElement('h2');
+    titleNode.textContent = book.title;
+    titleNode.classList.add('book-title');
+    bookNode.appendChild(titleNode);
+
+    const authorNode = document.createElement('p');
+    authorNode.textContent = 'by ' + book.author;
+    authorNode.classList.add('book-author');
+    bookNode.appendChild(authorNode);
+
+    const pagesNode = document.createElement('p');
+    pagesNode.textContent = book.pages + ' pages';
+    pagesNode.classList.add('book-pages');
+    bookNode.appendChild(pagesNode);
+
+    const readNode = document.createElement('p');
+    readNode.textContent = 'Read: ';
+    readNode.classList.add('book-read');
+    if (book.read) readNode.classList.add('checked');
+    function updateRead() {
+      book.toggleRead();
+      storeLibrary();
+      renderLibrary();
+    }
+    readNode.addEventListener('click', updateRead);
+    bookNode.appendChild(readNode);
+
+    const removeNode = document.createElement('p');
+    removeNode.textContent = 'Remove book';
+    removeNode.classList.add('book-remove', 'button');
+    function deleteBook() {
+      removeBook(i);
+      storeLibrary();
+      renderLibrary();
+    }
+    removeNode.addEventListener('click', deleteBook);
+    bookNode.appendChild(removeNode);
+
+    container.appendChild(bookNode);
   }
 
   const container = document.querySelector('#book-container');
+
+  function renderLibrary() {
+    clearNodes(container);
+    myLibrary.forEach(book => renderBook(book, container));
+  }
 
   function clearNodes(node) {
     [...node.childNodes].forEach(childNode => childNode.remove());
   }
 
-  function renderLibrary() {
-    clearNodes(container);
-    myLibrary.forEach((book, i) => book.render(container));
+  function storeLibrary() {
+    localStorage.setItem('library', JSON.stringify(myLibrary));
   }
 
-  document.querySelector('#new-book').addEventListener('click', () => {
+  document.querySelector('#new-book-button').addEventListener('click', () => {
     document.querySelector('.form').classList.toggle('hide');
   });
 
-  function addNewBook() {
+  function getNewBookFromForm() {
     const title = document.querySelector('#title-input').value;
     const author = document.querySelector('#author-input').value;
     const pages = document.querySelector('#pages-input').value;
@@ -118,7 +118,8 @@ function libraryMain() {
     const read = document.querySelector('#read-input').checked;
     if (title && author) {
       addBookToLibrary(title, author, pages, read);
-      myLibrary[myLibrary.length - 1].render(container);
+      storeLibrary();
+      renderLibrary();
       document.querySelector('.form').classList.add('hide');
       document.querySelector('#title-input').value = '';
       document.querySelector('#author-input').value = '';
@@ -128,7 +129,9 @@ function libraryMain() {
       alert("Title and author can't be empty.");
     }
   }
-  document.querySelector('#add-book').addEventListener('click', addNewBook);
+  document
+    .querySelector('#add-book-button')
+    .addEventListener('click', getNewBookFromForm);
 
   renderLibrary();
 }
